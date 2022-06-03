a5aae9a59ba47912ec67d18a12df37930719a0de
fikri <fuadfikri01@gmail.com>
2022-06-03 21:41:12 +0700

    add spring starter security

---
+++ src/main/java/com/syamsudin/springsecurity/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.syamsudin.springsecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/home")
+    public void home(){
+
+    }
+}
