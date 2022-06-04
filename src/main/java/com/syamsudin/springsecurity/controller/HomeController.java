19fe0dc4c75aabab14ac6b97ade8c8ac795a372a
fikri <fuadfikri01@gmail.com>
2022-06-05 05:47:21 +0700

    show current user login

--- src/main/java/com/syamsudin/springsecurity/controller/HomeController.java
+++ src/main/java/com/syamsudin/springsecurity/controller/HomeController.java
@@ -1,10 +1,13 @@
 package com.syamsudin.springsecurity.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+@Slf4j
 @Controller
 @RequestMapping()
 public class HomeController {
@@ -12,7 +15,11 @@ public class HomeController {
 
     @PreAuthorize("hasAuthority('EDIT_TRANSAKSI')")
     @RequestMapping(value = "/home", method = RequestMethod.GET)
-    public String home() {
+    public String home(Authentication authentication) {
+        log.info("user {}", authentication.getPrincipal());
+        log.info("jenis auth {}", authentication.getClass().getSimpleName());
+        log.info("user authorities {}", authentication.getAuthorities());
+
         return "home";
     }
 }
